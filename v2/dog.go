8fe669a40e0255155f5f013ae8993b6239b91166
acorn <w1013d27@126.com>
2023-08-05 20:50:29 +0800

    add dependency module Tmp3

--- v2/dog.go
+++ v2/dog.go
@@ -1,8 +1,12 @@
 package dog
 
+import "github.com/newacorn/tmp3/v2"
+
 func Bulk() {
 	println("wang wang form github.")
 	println("I am from v2 version.")
 	println("add!")
 	println("from main branch.")
+	tmp3.TmpVersion()
+
 }
--- v2/go.mod
+++ v2/go.mod
@@ -1,3 +1,5 @@
 module github.com/newacorn/dog/v2
 
 go 1.20
+
+require github.com/newacorn/tmp3/v2 v2.0.0
